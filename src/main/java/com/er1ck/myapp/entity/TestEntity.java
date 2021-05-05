091e8116f794a223742ffe48acb52a98c766440f
ErickCrz1508 <erickboss.513@gmail.com>
2021-05-05 18:20:59 -0500

    Nombre del Proyecto: my-app, Descripción: Version final del proyacto my-app

---
+++ src/main/java/com/er1ck/myapp/entity/TestEntity.java
@@ -0,0 +1,28 @@
+
+package com.er1ck.myapp.entity;
+
+/**
+ * @author Erick Michel Cruz Ceron
+ *
+ */
+public class TestEntity {
+
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo TestEntity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
